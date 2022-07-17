7a12f380b99bd64b787f97abde6326b493d82db1
Mochammad Fawwaz Islami <mailmcfawwaz@gmail.com>
2022-07-17 16:53:00 +0700

    add: spatie seeder

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -46,6 +46,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->asignRole('user');
+
         event(new Registered($user));
 
         Auth::login($user);
